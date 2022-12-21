af3074568aef746e017134f4940e5fda03298301
Amul <shindeamul76@gmail.com>
2022-12-21 19:05:42 +0530

    added more routes like forget pass resetpass etc..

--- route/post.js
+++ route/post.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const {
-    createPost, likeAndUnlikePost, deletePost, getPostOfFollowing, updateCaption,    
+    createPost, 
+    likeAndUnlikePost, 
+    deletePost, 
+    getPostOfFollowing, 
+    updateCaption, 
+    commentOnPost, 
+    deleteComment,    
 } 
 = require('../Controllers/post');
 const { isAuthenticated } = require('../middlewares/auth');
@@ -15,5 +21,9 @@ router.route("/post/:id")
 .delete(isAuthenticated, deletePost);
 
 router.route("/posts").get(isAuthenticated, getPostOfFollowing);
+
+router.route('/post/comment/:id')
+.put(isAuthenticated, commentOnPost)
+.delete(isAuthenticated, deleteComment);
 
 module.exports = router
--- route/user.js
+++ route/user.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { Register, Login, followeUser, logOut, updatePassword, updateProfile } = require('../Controllers/user');
+const { 
+    Register, 
+    Login, 
+    followeUser,
+    logOut, 
+    updatePassword, 
+    updateProfile, 
+    deleteProfile, 
+    myProfile,
+    getUserProfile,
+    getAllUsers,
+    forgetPassword,
+    resetPassword
+    } = require('../Controllers/user');
+
 const { isAuthenticated } = require('../middlewares/auth');
 
 router.route('/register').post(Register)
@@ -16,6 +30,17 @@ router.route('/update/password').put(isAuthenticated, updatePassword);
 
 router.route('/update/profile').put(isAuthenticated, updateProfile);
 
+router.route('/delete/me').delete(isAuthenticated, deleteProfile);
+
+router.route('/me').get(isAuthenticated, myProfile);
+
+router.route('/user/:id').get(isAuthenticated, getUserProfile);
+
+router.route('/users').get(isAuthenticated, getAllUsers);
+
+router.route('/forgot/password').post(forgetPassword);
+
+router.route('/password/reset/:token').put(resetPassword);
 
 
 module.exports = router
